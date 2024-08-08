9e1434da10b7159b1b8e3d8b825191223732f73d
Zhenyu_Pan <zhenyu_pan@cmu.ac.th>
2024-08-08 21:30:46 +0700

    DO 3

---
+++ src/views/event/EditView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { toRef} from 'vue'
+import { type Event } from '@/types'
+
+const props = defineProps<{
+    event: Event
+    id: String
+}>()
+// eslint-lisable-next-line @typescript-eslint/no-unused-vars
+const { event } = toRef(props)
+</script>
+
+<template>
+    <p>Edit event here</p>
+</template>
